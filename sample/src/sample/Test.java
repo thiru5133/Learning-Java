8aa40341040126398f402f9f96fa72e7d6a09f37
thiru <thiruma5133@gmail.com>
2025-09-14 18:16:58 +0530

    Initial commit: Learn GitHub workflow in Eclipse

---
+++ sample/src/sample/Test.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Test {
+	
+	static String topic="Learn github thing in eclipse";
+	
+    public static void main(String[] args) {
+        System.out.println(topic);
+    }
+}
